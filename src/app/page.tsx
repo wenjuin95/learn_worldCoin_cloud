1a0d5e82c02b5ddc49a01757b3bef692c02d21e1
low <lowwenjuin27@gmail.com>
2024-09-29 02:27:37 +0800

    [update]this repo is learn how button indicate with worldcoin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+"use client"; //is to indicate that the code will run in the client side
 
+import { useState } from "react";
 import { VerificationLevel, IDKitWidget, useIDKit } from "@worldcoin/idkit";
 import type { ISuccessResult } from "@worldcoin/idkit";
 import { verify } from "./actions/verify";
 
 export default function Home() {
+  const [proofResult, setProofResult] = useState<string | null>(null);
   const app_id = process.env.NEXT_PUBLIC_WLD_APP_ID as `app_${string}`;
   const action = process.env.NEXT_PUBLIC_WLD_ACTION;
 
@@ -15,8 +17,9 @@ export default function Home() {
     throw new Error("action is not set in environment variables!");
   }
 
-  const { setOpen } = useIDKit();
+  const { setOpen } = useIDKit(); 
 
+  // This function is called when a user has been successfully verified
   const onSuccess = (result: ISuccessResult) => {
     // This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
     window.alert(
@@ -25,16 +28,24 @@ export default function Home() {
     );
   };
 
+  // This function is called when a user has been successfully verified and the proof has been sent to the backend
   const handleProof = async (result: ISuccessResult) => {
     console.log(
       "Proof received from IDKit, sending to backend:\n",
       JSON.stringify(result)
     ); // Log the proof from IDKit to the console for visibility
+
+    // Extract only the nullifier hash from the proof to display to the user
+    const formattedResult = `ID: ${result.nullifier_hash}`;
+	setProofResult(formattedResult); // Set the proof result in the state for display
+
+	//get the proof from the backend (verify the proof)
     const data = await verify(result);
-    if (data.success) {
+    if (data.success == true) { //check if the verification was success or not
       console.log("Successful response from backend:\n", JSON.stringify(data)); // Log the response from our backend for visibility
-    } else {
-      throw new Error(`Verification failed: ${data.detail}`);
+    } 
+	else {
+      throw new Error(`${data.detail}`); // Throw an error if the verification failed
     }
   };
 
@@ -43,11 +54,11 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center align-middle h-screen">
         <p className="text-2xl mb-5">World ID Cloud Template</p>
         <IDKitWidget
-          action={action}
-          app_id={app_id}
-          onSuccess={onSuccess}
-          handleVerify={handleProof}
-          verification_level={VerificationLevel.Orb} // Change this to VerificationLevel.Device to accept Orb- and Device-verified users
+          action={action} //action to use for verification
+          app_id={app_id} //app ID to use for verification
+          onSuccess={onSuccess} //function to call when a user has been successfully verified
+          handleVerify={handleProof} //function to call when a user has been successfully verified and the proof has been sent to the backend
+          verification_level={VerificationLevel.Device} // Change this to VerificationLevel.Device to accept Orb- and Device-verified users
         />
         <button
           className="border border-black rounded-md"
@@ -55,6 +66,11 @@ export default function Home() {
         >
           <div className="mx-3 my-1">Verify with World ID</div>
         </button>
+        {proofResult && ( //check if the proofResult is not null and then display the proofResult
+          <pre>
+            {proofResult}
+          </pre>
+        )}
       </div>
     </div>
   );
